f476c90a67c3b9383142fd729fb6a912c76d0b38
JonasPequeno <Jonaspdasilva31@gmail.com>
2019-08-20 21:27:39 -0300

    Implementado listagem de devs, funçoes de like e dislike

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,97 +1,73 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './Main.css';
 
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import api from '../services/api';
 
-export default function Main() {
+export default function Main({ match }) {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function loadUsers() {
+            const res = await api.get('/devs', {
+                headers: { user: match.params.id }
+            })
+            setUsers(res.data);
+        }
+        loadUsers();
+    }, [match.params.id]);
+
+    async function handleLike(id) {
+        await api.post(`devs/${id}/likes`, null, {
+            headers: { user: match.params.id }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
+    async function handleDislike(id) {
+        await api.post(`devs/${id}/dislikes`, null, {
+            headers: { user: match.params.id }
+        });
+
+        setUsers(users.filter(user => user._id !== id));
+    }
+
     return (
         <div className="main-container">
             <img src={logo} alt="Tindev" />
-            <ul>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/19806594?v=4" alt="" />
-                    <footer>
-                        <strong>Jonas Pequeno</strong>
-                        <p>Graduando em Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras, Consultor Jr. em @loopisjr</p>
-                    </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/19806594?v=4" alt="" />
-                    <footer>
-                        <strong>Jonas Pequeno</strong>
-                        <p>Graduando em Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras, Consultor Jr. em @loopisjr</p>
-                    </footer>
+            {users.length > 0 ? (
+                <ul>
+                    {users.map(user => (
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/19806594?v=4" alt="" />
-                    <footer>
-                        <strong>Jonas Pequeno</strong>
-                        <p>Graduando em Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras, Consultor Jr. em @loopisjr</p>
-                    </footer>
+                        <li key={user._id}>
+                            <img src={user.avatar} alt="{user.name}" />
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/19806594?v=4" alt="" />
-                    <footer>
-                        <strong>Jonas Pequeno</strong>
-                        <p>Graduando em Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras, Consultor Jr. em @loopisjr</p>
-                    </footer>
+                            <footer>
+                                <strong>{user.name}</strong>
+                                <p>{user.bio}</p>
+                            </footer>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-                <li>
-                    <img src="https://avatars2.githubusercontent.com/u/19806594?v=4" alt="" />
-                    <footer>
-                        <strong>Jonas Pequeno</strong>
-                        <p>Graduando em Análise e Desenvolvimento de Sistemas - IFPB - Campus Cajazeiras, Consultor Jr. em @loopisjr</p>
-                    </footer>
+                            <div className="buttons">
+                                <button type="button" onClick={() => handleDislike(user._id)}>
+                                    <img src={dislike} alt="dislike" />
+                                </button>
 
-                    <div className="buttons">
-                        <button type="button">
-                            <img src={dislike} alt="dislike" />
-                        </button>
-                        <button type="button">
-                            <img src={like} alt="like" />
-                        </button>
-                    </div>
-                </li>
-            </ul>
+                                <button type="button" onClick={() => handleLike(user._id)}>
+                                    <img src={like} alt="like" />
+                                </button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                    <div className="empty">Acabou :(</div>
+                )}
         </div>
     )
 }
